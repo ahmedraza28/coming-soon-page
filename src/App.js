93a7fe680d3b103268c1c3e6f069a1f061ef68ab
Ahmed Raza <88032839+ahmedraza28@users.noreply.github.com>
2024-08-03 15:25:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <video autoPlay muted loop id="bg-video">
+//         <source src={`${process.env.PUBLIC_URL}/night_sky.mp4`} type="video/mp4" />
+//         Your browser does not support HTML5 video.
+//       </video>
+//       <header className="App-header">
+//         <h1>Nixor Tech Solutions</h1>
+//         <p>Launching Soon</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <video autoPlay muted loop id="bg-video">
+      <video autoPlay muted loop playsInline id="bg-video">
         <source src={`${process.env.PUBLIC_URL}/night_sky.mp4`} type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
@@ -17,6 +37,7 @@ function App() {
 }
 
 export default App;
+
 
 // import React, { useEffect } from 'react';
 // import './App.css';
